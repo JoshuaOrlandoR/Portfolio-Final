fc4ae37b0288f0e35cd5bdd30a3fbe12ddb40956
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2023-06-15 15:44:23 -0400

    Finished making all components, styling changes next and fixing hero buttons, will try to add animations to scroll as well

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -3,10 +3,20 @@ import './styles.css';
 
 const Contact = () => {
     return (
-        <div className="contact-container">
-            <h1>Contact Component</h1>
+        <div id="contact" className="contact-container">
+            <div className="title-wrapper">
+                <h2 className="contact-title">Contact</h2>
+            </div>
+            <div className="contact-wrapper">
+                <form className="contact-form">
+                    <input type="text" placeholder="Name" required />
+                    <input type="email" placeholder="Email" required />
+                    <textarea placeholder="Message" required></textarea>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
-    )
-};
+    );
+}
 
 export default Contact;
--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React from "react";
 import './styles.css';
+import { FaArrowUp, FaGithub, FaLinkedin, FaYoutube, FaRegFileAlt } from 'react-icons/fa';
+import { animateScroll as scroll } from 'react-scroll';
+
 
 const Footer = () => {
-    return (
-        <div className="footer-container">
-            <h1>Footer Component</h1>
-        </div>
-    )
+
+    const scrollToTop = () => {
+        scroll.scrollToTop({duration: 1100});
+      }
+
+  return (
+    <div className="footer-container">
+      <button className="scroll-top" onClick={scrollToTop}><FaArrowUp /></button>
+      <div className="footer-social-container">
+        <a href="https://github.com/" target="_blank" rel="noreferrer">
+          <FaGithub className="footer-icon" />
+        </a>
+        <a href="https://linkedin.com/" target="_blank" rel="noreferrer">
+          <FaLinkedin className="footer-icon" />
+        </a>
+        <a href="https://youtube.com/" target="_blank" rel="noreferrer">
+          <FaYoutube className="footer-icon" />
+        </a>
+        <a href="https://resume.com/" target="_blank" rel="noreferrer">
+          <FaRegFileAlt className="footer-icon" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
